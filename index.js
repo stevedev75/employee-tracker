ba1c1ccbc6f71625b250fb32aa32cdbb363a7b86
Steve Dever <stephendever.cr@gmail.com>
2021-05-20 15:18:30 -0400

    update 'add' functions less console.table

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ const addData = () => {
      choices: [
        'Departments',
        'Roles',
-       'Exployees',  
+       'Employees',  
      ],
   })
   .then((answer) => {
@@ -74,7 +74,7 @@ const addData = () => {
       addRole();
        break;
        
-     case 'Exployees':
+     case 'Employees':
       addEmp();
        break;  
 
@@ -85,13 +85,17 @@ const addData = () => {
   });
 };
 
+
+// USE THE CONSOLE.TABLE before we move ahead??
+// If so, when do I do the JOINs?? -- it must be before this step, right?
+
 // Build the above 3 mentioned functions below
-// addDept
-// addRole
-// addEmp
+// addDept X
+// addRole X
+// addEmp X
 // Remember to use 'console.table' for output!
 
-// My attempt at writing the addDept function
+// The addDept function
 const addDept = () => {
   inquirer
    .prompt({
@@ -99,9 +103,96 @@ const addDept = () => {
       type: 'input',
        message: 'Name of new department?',   
    })
-    .then
+   .then(function(res) {
+     const department = res.department;
+     const query = `INSERT INTO department (name) VALUES("${department}")`;
+      connection.query(query, function(err, res) {
+       if (err) throw err;
+     //   console.table(????) // How do I "print out the updated table?"
+      runSearch();
+    });
+  });
+}
+// End of addDept function    
 
-// End of addDept function attempt    
+// The addRole function
+const addRole = () => {
+  inquirer
+   .prompt([
+     {
+     name: 'title',
+      type: 'input',
+       message: 'Name of new role title?',   
+     },
+
+     {
+     name: 'salary',
+      type: 'input',
+       message: 'Salary of the new role?',   
+     },
+
+     {
+      name: 'department_id',
+       type: 'input',
+        message: 'Department ID of the new role?',   
+     },
+    ])
+     .then(function(res) {
+      const title = res.title;
+      const salary = res.salary;
+      const department_id = res.department_id;
+      const query = `INSERT INTO role (title, salary, department_id) VALUE("${title}", "${salary}", "${department_id}")`;
+      connection.query(query, function(err, res) {
+        if (err) throw err;
+        // console.table(????) // How do I "print out the updated table?"
+      runSearch();
+    });
+  })
+};
+// End of addDept function 
+
+// The addEmp function
+const addEmp = () => {
+  inquirer
+   .prompt([
+     {
+     name: 'first_name',
+      type: 'input',
+       message: 'First Name of new employee?',   
+     },
+
+     {
+     name: 'last_name',
+      type: 'input',
+       message: 'Last name of new employee?',   
+     },
+
+     {
+      name: 'role_id',
+       type: 'input',
+        message: 'Role ID of new employee?',   
+     },
+
+     {
+      name: 'manager_id',
+       type: 'input',
+        message: 'Manager ID of new employee?',   
+     },
+    ])
+    .then(function(res) {
+      const first_name = res.first_name;
+      const last_name = res.last_name;
+      const role_id = res.role_id;
+      const manager_id = res.manager_id;
+      const query = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUE("${first_name}", "${last_name}", "${role_id}", "${manager_id}")`;
+      connection.query(query, function(err, res) {
+        if (err) throw err;
+      // console.table(????) // How do I "print out the updated table?"
+      runSearch();
+    });
+  });
+}
+// End of addDept function 
 
 const viewData = () => {
     inquirer
@@ -111,7 +202,7 @@ const viewData = () => {
         choices: [
           'Departments',
           'Roles',
-          'Exployees',  
+          'Employees',  
         ],
      })
      .then((answer) => {
@@ -124,7 +215,7 @@ const viewData = () => {
          viewRole();
           break;
           
-        case 'Exployees':
+        case 'Employees':
          viewEmp();
           break;  
    
@@ -134,16 +225,32 @@ const viewData = () => {
        }  
      });
    };
-
+  
 // Build the above 3 mentioned functions below
-// viewDept
+// viewDept X
 // viewRole
 // viewEmp
 // Remember to use 'console.table' for output!
+
+// Beginning of viewDept function
+
+function viewDept() {
+  var query = "SELECT * FROM department";
+    connection.query(query, function(err, res) {
+        console.log(`DEPARTMENTS:`)
+      res.forEach(department => {
+          // console.log(`ID: ${department.id} | Name: ${department.name}`)
+           // console.table(????) // How do I "print out the updated table?"
+      })
+      runSearch();
+      });
+  };
+
+// End of viewDept function
+
 
 
 
 
 
 // Build the updateData function below
-// Remember to use 'console.table' for output!
